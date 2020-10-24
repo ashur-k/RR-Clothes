0f1c7f45a6d9ab5316e83c2e985b27575a47881a
ashur-k <ashurkanwal@yahoo.com>
2020-10-24 15:01:01 +0000

    products admin.py updated, product variants tabular inline are added.

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,6 @@
+import admin_thumbnails
 from django.contrib import admin
-from .models import Category, Product, Images
+from .models import Category, Product, Images, Color, Size, Variants
 
 
 # Register your models here.
@@ -10,18 +11,47 @@ class CategoryAdmin(admin.ModelAdmin):
     )
 
 
+@admin_thumbnails.thumbnail('image')
 class ProductImageInline(admin.TabularInline):
     model = Images
-    extra = 5
+    readonly_fields = ('id',)
+    extra = 1
+
+
+class ProductVariantsInline(admin.TabularInline):
+    model = Variants
+    readonly_fields = ('image_tag',)
+    extra = 1
+    show_change_link = True
+
+
+@admin_thumbnails.thumbnail('image')
+class ImagesAdmin(admin.ModelAdmin):
+    list_display = ('image', 'title', 'image_thumbnail')
 
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'category', 'status', 'image_tag', ]
     list_filter = ['category']
     readonly_fields = ('image_tag',)
-    inlines = [ProductImageInline]
+    inlines = [ProductImageInline, ProductVariantsInline]
+
+
+class ColorAdmin(admin.ModelAdmin):
+    list_display = ['name', 'code', 'color_tag']
+
+
+class SizeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'code']
+
+
+class VariantsAdmin(admin.ModelAdmin):
+    list_display = ['title', 'product', 'color', 'size', 'price', 'quantity', 'image_tag']
 
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product, ProductAdmin)
-admin.site.register(Images)
+admin.site.register(Images, ImagesAdmin)
+admin.site.register(Color, ColorAdmin)
+admin.site.register(Size, SizeAdmin)
+admin.site.register(Variants, VariantsAdmin)
