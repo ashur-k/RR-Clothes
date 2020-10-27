8d2bd366dc0d5be565063c9c7a6db0769893f08e
ashur-k <ashurkanwal@yahoo.com>
2020-10-27 14:57:51 +0000

    adding logic for bag, added context.py for context processor

--- RR_Clothes/settings.py
+++ RR_Clothes/settings.py
@@ -75,6 +75,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # required to access bag contents across the entire site
+                'shopping_bag.contexts.bag_contents'
             ],
         },
     },
@@ -157,6 +159,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
 
 ####################################
 # CKEDITOR CONFIGURATION ##
