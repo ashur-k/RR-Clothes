ef0ffaced673d6eb2507f5c572ed48d52888139d
ashur-k <ashurkanwal@yahoo.com>
2020-12-11 12:35:00 +0000

    set secret key and debug variable in settings

--- RR_Clothes/settings.py
+++ RR_Clothes/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6-c*(ua@=55ifhe3#4=of=k@^pt!=oqj7g$qfd(+)k@6g0k89n'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['rr-cloth.herokuapp.com', 'localhost']
 
